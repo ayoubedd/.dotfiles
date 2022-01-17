94472e8a204e7c73033daaa316b59a5ea503f9de
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2022-01-17 05:48:08 +0100

    neovim: remove auto-tag plugin

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -175,16 +175,6 @@ lvim.plugins = {
 		end,
 	},
 
-
-  {
-  "windwp/nvim-ts-autotag",
-  event = "InsertEnter",
-  config = function()
-    require("nvim-ts-autotag").setup()
-  end,
-},
-
-
 {
   "norcalli/nvim-colorizer.lua",
     config = function()
