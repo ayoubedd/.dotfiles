6cf0ddffd7c45c70f0704a590dc35f0550374a8d
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2022-01-20 06:24:33 +0100

    lvim: update config

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -256,23 +256,27 @@ lvim.plugins = {
     table.insert(dial.config.searchlist.normal, "custom#Boolean")
   end,
 },
-
-
-{
-  "lukas-reineke/indent-blankline.nvim",
-  event = "BufRead",
-  setup = function()
-    vim.g.indentLine_enabled = 1
-    vim.g.indent_blankline_char = "▏"
-    vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-    vim.g.indent_blankline_buftype_exclude = {"terminal"}
-    vim.g.indent_blankline_show_trailing_blankline_indent = false
-    vim.g.indent_blankline_show_first_indent_level = false
-  end
-},
-
-
 }
+
+
+local linters = require "lvim.lsp.null-ls.linters"
+
+linters.setup {
+  {
+    exe = "eslint_d",
+    filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  },
+}
+
+-- formatters.setup {
+--   -- { exe = "black" },
+--   {
+--     exe = "prettier",
+--     args = { "--print-width", "100" },
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
